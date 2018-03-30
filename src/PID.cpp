56e63c0581636a1e8010d627e498c7df22ce489d
reshav <singla.reshav@gmail.com>
2018-03-30 15:26:34 -0700

    initial

---
+++ src/PID.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <cmath>
+#include "PID.h"
+
+using namespace std;
+
+
+/*
+* TODO: Complete the PID class.
+*/
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp, double Ki, double Kd) {
+    prev_cte = -1.0;
+    total_cte = 0.0;
+    prev_prev_cte = 0.0;
+    best_err = 0.0;
+    throttle = 0.3;
+
+    params = {Kp,Kd,Ki};
+    params_factor = {0,0,0};
+    params_error = {.003,1,0.000003};
+    current_error = 0;
+}
+
+void PID::UpdateError(double cte) {
+    /*
+    double learning_rate = 0.1;
+    Kp = Kp + learning_rate * (cte - prev_cte);
+    Ki = Ki + learning_rate * (cte);
+    Kd = Kd + learning_rate * (prev_cte - prev_prev_cte);
+    prev_prev_cte = prev_cte;
+     */
+    int i = current_error;
+    double param = params[i];
+    double param_factor = params_factor[i];
+    double param_error = params_error[i];
+    double err = pow(cte,2);
+    if (param_factor == 0) {
+        param = param + param_error;
+        param_factor = 1;
+    } else if (param_factor == 1) {
+        if (err < best_err) {
+            best_err = err;
+            param_factor = 0;
+            param_error = 1.1 * param_error;
+            current_error = (current_error + 1)%3;
+        } else {
+            param_factor = -1;
+            param = param - 2 * param_error;
+        }
+    } else if (param_factor == -1) {
+        if (err < best_err) {
+            best_err = err;
+            param_factor = 0;
+            param_error = 1.1 * param_error;
+            current_error = (current_error + 1)%3;
+        } else {
+            param_factor = 0;
+            param = param + param_error;
+            param_error = 0.9 * param_error;
+            current_error = (current_error + 1)%3;
+        }
+    }
+
+    params[i] = param;
+    params_factor[i] = param_factor;
+    params_error[i] = param_error;
+
+}
+
+double PID::TotalError() {
+}
+
---
+++ src/PID.h
@@ -0,0 +1,43 @@
+#ifndef PID_H
+#define PID_H
+#include "vector"
+
+class PID {
+public:
+  double prev_prev_cte;
+  double prev_cte;
+  double total_cte;
+  double best_err;
+  double throttle;
+
+  std::vector<double> params;
+  std::vector<double > params_factor;
+  std::vector<double > params_error;
+  int current_error;
+    /*
+  * Constructor
+  */
+  PID();
+
+  /*
+  * Destructor.
+  */
+  virtual ~PID();
+
+  /*
+  * Initialize PID.
+  */
+  void Init(double Kp, double Ki, double Kd);
+
+  /*
+  * Update the PID error variables given cross track error.
+  */
+  void UpdateError(double cte);
+
+  /*
+  * Calculate the total PID error.
+  */
+  double TotalError();
+};
+
+#endif /* PID_H */
